80bb9323e5149b60c02495dd99995102cd90800d
Lucas Copque <copque.faculdade@gmail.com>
2019-04-23 13:20:42 -0300

    método de update empresa do usuário

--- src/main/java/br/com/incentivados/service/UsuarioService.java
+++ src/main/java/br/com/incentivados/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.incentivados.service;
 
+import br.com.incentivados.model.Empresa;
 import br.com.incentivados.model.Usuario;
 import br.com.incentivados.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,39 +21,53 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
     }
 
+    // Método para verificar a existência do usuário e senha
     public boolean existsByEmailAndSenha(String cpf, String senha){
         return usuarioRepository.existsByEmailAndSenha(cpf, senha);
     }
-    
+
+    // Método para verificar a existência do cpf
     public boolean existsByCpf(String cpf){
         return usuarioRepository.existsByCpf(cpf);
     }
-    
+
+    // Método para verificar a existência do email
     public boolean existsByEmail(String email){
         return usuarioRepository.existsByEmail(email);
     }
 
+    // Método para validação de login
     public Usuario login(String email){
     	Usuario usuario = usuarioRepository.findByEmail(email);
     	ultimoAcesso(usuario);
         return usuario;
     }
-    
+
+    // Método para atualizar o último acesso do usuário
     public Usuario ultimoAcesso(Usuario usuario) {
     	usuario.setUltimoAcesso(new Date());
     	return usuarioRepository.save(usuario);
     }
-        
-    public Optional<Usuario> findById(Long id) {
-    	return usuarioRepository.findById(id);
+
+    // Método para atualizar a empresa do usuário
+    public Usuario setEmpresa(Usuario usuario, Empresa empresa) {
+        usuario.setEmpresa(empresa);
+        return usuarioRepository.save(usuario);
     }
-    
+
+    // Método para buscar um usuário por id
+    public Optional<Usuario> findById(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    // Método para listar todos usuários
     public List<Usuario> findAll(){
-    	return usuarioRepository.findAll();
-    }   
-    
+        return usuarioRepository.findAll();
+    }
+
+    // Método para listar todos os usuários que conter os caracteres passados como parâmetro
     public List<Usuario> findByNomeContains(String nome){
-    	return usuarioRepository.findByNomeContains(nome);
+        return usuarioRepository.findByNomeContains(nome);
     }
 
 }
