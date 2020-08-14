28f416e26456b34bc254f46c44cf3bd8d8692a5f
Lucas Copque <copque.faculdade@gmail.com>
2020-08-14 12:03:33 -0300

    fixed bug file validator

--- src/main/resources/static/assets/js/file-validator.js
+++ src/main/resources/static/assets/js/file-validator.js
@@ -1,51 +1,46 @@
 var limite_bytes = 20971520 // 20MB
 
 function validaImg($input, id) {
-  var extPermitidas = ['jpg',, 'jpeg', 'png', 'bmp'];
+  var extPermitidas = ['jpg', 'jpeg', 'png', 'bmp'];
   var extArquivo = $input.value.split('.').pop();
-  var seletor = $(id);
 
   if($input.files[0].size > limite_bytes){
     $('#arquivo-recusado-tamanho').modal('show');
-    $(seletor).val('');
+    $('#' + id).val('');
   }
 
   if(typeof extPermitidas.find(function(ext){ return extArquivo.toLowerCase() === ext }) == 'undefined') {
     $('#arquivo-recusado-img').modal('show');
-    $(seletor).val('');
+    $('#' + id).val('');
   }
 }
 
 function validaPdf($input, id) {
   var extPermitidas = ['pdf'];
   var extArquivo = $input.value.split('.').pop();
-  var seletor = $(id);
-
-  console.log(seletor);
 
   if($input.files[0].size > limite_bytes){
     $('#arquivo-recusado-tamanho').modal('show');
-    $(seletor).val('');
+    $('#' + id).val('');
   }
 
   if(typeof extPermitidas.find(function(ext){ return extArquivo.toLowerCase() === ext }) == 'undefined') {
-    $('#arquivo-recusado-pdf').modal('show');    
-    $(seletor).val('');
+    $('#arquivo-recusado-pdf').modal('show');
+    $('#' + id).val('');
   }
 }
 
 function validaImgPdf($input, id) {
-  var extPermitidas = ['jpg',, 'jpeg', 'png', 'bmp', 'pdf'];
+  var extPermitidas = ['jpg', 'jpeg', 'png', 'bmp', 'pdf'];
   var extArquivo = $input.value.split('.').pop();
-  var seletor = $(id);
 
   if($input.files[0].size > limite_bytes){
     $('#arquivo-recusado-tamanho').modal('show');
-    $(seletor).val('');
+    $('#' + id).val('');
   }
 
   if(typeof extPermitidas.find(function(ext){ return extArquivo.toLowerCase() === ext; }) == 'undefined') {
-    $('#arquivo-recusado-all').modal('show');  
-    $(seletor).val('');
+    $('#arquivo-recusado-all').modal('show');
+    $('#' + id).val('');
   }
 }
